297e1087b1b31886b924d655edd97227870ee6a9
xs3508198 <1131259489@qq.com>
2026-04-02 20:39:17 +0800

    feat: add service discovery

--- ozhera-mind/ozhera-mind-worker/ozhera-mind-worker-server/src/main/java/org/apache/ozhera/mind/server/config/NacosConfiguration.java
+++ ozhera-mind/ozhera-mind-worker/ozhera-mind-worker-server/src/main/java/org/apache/ozhera/mind/server/config/NacosConfiguration.java
@@ -18,14 +18,29 @@
  */
 package org.apache.ozhera.mind.server.config;
 
+import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 @Configuration
 @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "${nacos.config.address}"))
 @NacosPropertySource(dataId = "hera_mind_config", autoRefreshed = true)
 public class NacosConfiguration {
 
+    @Value("${nacos.config.address:127.0.0.1:8848}")
+    private String nacosAddress;
+
+    @Bean("nacosNamingService")
+    public NamingService namingService() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("serverAddr", nacosAddress);
+        return NacosFactory.createNamingService(properties);
+    }
 }
